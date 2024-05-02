f0bfc976c944ce2873b29f8fd7dc21e71e6c216b
Juan Soto <juansoto@Mac-mini-de-Juan.local>
2024-05-02 19:34:47 +0100

    Initial Commit

---
+++ src/main/java/com/myBank/rickandmortybank/RickAndMortyBankApplication.java
@@ -0,0 +1,13 @@
+package com.myBank.rickandmortybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickAndMortyBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myBank/rickandmortybank/RickAndMortyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myBank.rickandmortybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
